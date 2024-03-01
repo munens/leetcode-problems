0202267c51ed7255778cbe912f864f4f2ed9ad50
Munene Kaumbutho <{ID}+{username}@users.noreply.github.com>
2024-03-01 08:42:58 -0500

    main: added new exercises. Minor changes required to come up with ideal solutions

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -4,7 +4,6 @@ package main
 import "fmt"
 
 func majorityElement(nums []int) int {
-	maxV := 0
 	numsMap := make(map[int]int, len(nums))
 	for _, v := range nums {
 		if _, ok := numsMap[v]; ok {
@@ -12,15 +11,19 @@ func majorityElement(nums []int) int {
 		} else {
 			numsMap[v] = 1
 		}
-
-		if numsMap[v] > maxV {
-			maxV = v
-		}
 	}
 
 	fmt.Println(numsMap)
+	maxV := 0
+	elem := 0
+	for i, v := range numsMap {
+		if val, _ := numsMap[v]; val > maxV {
+			maxV = numsMap[v]
+			elem = i
+		}
+	}
 
-	return maxV
+	return elem
 }
 
 func main() {
@@ -29,4 +32,7 @@ func main() {
 
 	v1 := majorityElement([]int{2, 2, 1, 1, 1, 2, 2, 3, 3, 3, 6, 7, 7, 7, 7, 7})
 	fmt.Println("v1 = ", v1)
+
+	v3 := majorityElement([]int{6, 5, 5})
+	fmt.Println("v3 = ", v3)
 }
--- 189-rotate-array/rotate-array.go
+++ 189-rotate-array/rotate-array.go
@@ -4,8 +4,13 @@ package main
 import "fmt"
 
 func rotate(nums []int, k int) {
+
+	if len(nums) == 1 || k == 0 {
+		return
+	}
 	fmt.Println("nums = ", nums, ", k = ", k)
-	thresh := len(nums) - k
+	initThresh := len(nums) - k
+	thresh := initThresh
 	fmt.Println("threshI = ", thresh, ", threshV = ", nums[thresh])
 	for i, _ := range nums {
 		if thresh < len(nums) {
@@ -15,15 +20,81 @@ func rotate(nums []int, k int) {
 			nums[thresh] = currV
 			thresh++
 		} else {
-			// handle array after thresh index
+			break
 		}
 	}
 
-	fmt.Println(nums)
+	fmt.Println("init nums: ", nums)
+
+	i := k
+	j := initThresh
+	finIndex := len(nums) - 1
+
+	for {
+		if i > finIndex {
+			break
+		}
+
+		if j > finIndex {
+			j = finIndex
+		}
+
+		vi := nums[i]
+		vj := nums[j]
+
+		nums[i] = vj
+		nums[j] = vi
+
+		i++
+		j++
+	}
+
+	fmt.Println("final nums: ", nums)
+}
+
+func rotate2(nums []int, k int) {
+	rotateIndex := k % len(nums)
+	threshIndex := len(nums) - rotateIndex
+	indexed := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		if i < rotateIndex {
+			indexed[i] = threshIndex
+			threshIndex++
+		} else {
+			indexed[i] = i - rotateIndex
+		}
+	}
+
+	fmt.Println(indexed)
 }
 
 func main() {
+	//v0 := []int{1, 2, 3, 4, 5, 6, 7}
+	//rotate(v0, 3)
+	//fmt.Printf("v0 = %d \n\n", v0)
+	//
+	//v1 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	//rotate(v1, 9)
+	//fmt.Printf("v1 = %d \n\n", v1)
+
+	// 2/27: some cases to handle
+	// - what happens if k > len(nums) ? How to handle this use case
+	// - Need to go through algorithm right after line 27, does it work for all cases? It appears not to
+
 	v0 := []int{1, 2, 3, 4, 5, 6, 7}
-	rotate(v0, 3)
-	fmt.Println("v0 = ", v0)
+	rotate2(v0, 3)
+
+	v1 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	rotate2(v1, 4)
+
+	v2 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	rotate2(v2, 199)
+
+	// 3/1: look at rotate2
+	// - I have solved the problem of k > len(nums). We use modulo to find the remainder of the division of k and len(nums). This gives
+	//   a different value of k that then allows us to know where to start rotating the array.
+	// - I have created an indexed array above. It shows the correct indexes for the array that is supposed to be indexed.
+	// - need to use the indexed array to set the nums array value in the right place, and this should be easy 2*O(n)
+
 }
