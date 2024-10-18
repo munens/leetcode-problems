527505eb340017fab03b77cbba37499f2748b113
Munene Kaumbutho <munene.kaumbutho@workjam.com>
2024-10-17 23:38:07 -0400

    main: h-index

---
+++ 274-h-index/h-index.go
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/h-index/description/?envType=study-plan-v2&envId=top-interview-150
+package main
+
+func hIndex(citations []int) int {
+	constraint := len(citations)
+}
+
+func main() {}
